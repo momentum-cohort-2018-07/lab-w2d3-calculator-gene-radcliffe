32e59c7dbab4fe9bbf0217e68647f68867e1e9bd
gene-radcliffe <g_radcliffe82@hotmail.com>
2018-08-08 20:54:26 -0400

    Committing Calculator with a few bugs

--- main.js
+++ main.js
@@ -1,71 +1,118 @@
 var equal_pushed = false;
 var dot_pushed =false;
+var sign_pushed = false;
+var initial = true;
+var input ="";
 //accumulator function
 displayAccumulator();
+function clearScreen(reset){
+   
+    var screen = document.querySelector(".screen");
+
+    if(reset){
+        screen.innerText ="0";
+    }else{
+        screen.innerText= ""
+    }
+   return;
+}
 function displayAccumulator(){
     
     var buttons = document.querySelectorAll('.numeral-section--button');
+    var signButtons = document.querySelectorAll('.sign');
+    var  screen = document.querySelector(".screen"); // accumulator
+
+    for(var sign of signButtons){
+        sign.addEventListener("click", function(e){
+        var buttonS = e.srcElement;
+        var signButton = buttonS.innerText;
+
+            if(!sign_pushed){
+                console.log("pushed sign " + signButton)
+                switch(signButton){
+                    case "+":
+                        
+                        console.log("pushed addition")
+                        screen.innerText+=signButton;
+                        sign_pushed=true
+                        equal_pushed = false
+                     break;
+                    case "-":
+                        console.log("pushed substract")
+                        screen.innerText+=signButton;
+                        sign_pushed=true
+                        equal_pushed = false
+                        break;
+                    case "/":
+                        console.log("pushed division")
+                        screen.innerText+=signButton;
+                        sign_pushed=true
+                        equal_pushed = false
+                        break;
+                    case "*":
+                        console.log("pushed multipy")
+                        screen.innerText+=signButton;
+                        sign_pushed=true
+                        equal_pushed = false
+                        break;
+                }
+    
+            } 
+        })
+    }
+    
     for(var button of buttons){
         button.addEventListener("click", function(e){
             var  button = e.srcElement;
-            var  screen = document.querySelector(".screen"); // accumulator
-            var  input = button.innerText;
+           
+            input = button.innerText;
             var  output ='';
            
             console.log(input)
             switch(input){
                 case "C":
-                    screen.innerText=" ";
+                    clearScreen(true);
                     break;
                 case "+":
-                if(equal_pushed===true){
-                    equal_pushed=false;
-                }
-                    screen.innerText+=input;
                     break;
                 case "-":
-                    if(equal_pushed===true){
-                        equal_pushed=false;
-                    }
-                        screen.innerText+=input;
                     break;
                 case "/":
-                    if(equal_pushed===true){
-                        equal_pushed=false;
-                    }
-                        screen.innerText+=input;
                     break;
                 case "*":
-                    if(equal_pushed===true){
-                        equal_pushed=false;
-                    }
-                        screen.innerText+=input;
                     break;
                 case ".":
-                    if(equal_pushed===true){
-                        equal_pushed=false;
-                    }
+                
                     if(dot_pushed===false){
+                        console.log("dot is pushed")
                         screen.innerText+=input;
-                        dot_pushed===true;
-                    }
+                        dot_pushed=true;
                         
+                    }
+                  
                     break;
                 case "=":
+                   if(!initial){
                     equal_pushed=true;
                     output = screen.innerText;
                     screen.innerText=""
-                    console.log("acc: " + output);
                     output = eval(output);
                     console.log(output)
                     screen.innerText = output;
+                   }
                     break;
-                default:    
+                default: 
+                    if(initial){
+                        clearScreen(false)
+                    }
                     if(equal_pushed===true){
-                        screen.innerText=" "
+                        screen.innerText = ""
                         equal_pushed=false;
+                        break
                     }
                     screen.innerText+=input;
+                    sign_pushed=false;
+                    initial = false;
             }
           })
     }
